7ca8f012701e58fb92d1767eb33894a1bcebca87
sam <cjsam6@yahoo.com>
2024-12-16 19:49:24 +0800

    ✨ database entity

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,10 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Date Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
