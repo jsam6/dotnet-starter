dcb770d3965f0010b5ae4bcf8ef69d94fcf982f4
sam <cjsam6@yahoo.com>
2024-12-28 16:17:35 +0800

    ✨ dto mapping

---
+++ GameStore.Api/Mapping/GameMapping.cs
@@ -0,0 +1,20 @@
+using GameSt.Api.Entities;
+using GameStore.Api.Dto;
+
+namespace GameStore.Api.Mapping;
+
+public static class GameMapping {
+    public static Game ToEntity(this CreateGameDto  game) {
+        return new Game() {
+            Name = game.Name,
+            GenreId = game.GenreId,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+    }
+
+    
+    public static GameDto ToDto(this Game game) {
+        return new(game.Id, game.Name, game.Genre!.Name, game.Price, game.ReleaseDate);
+    }
+}
